e3a85a8a80544b835f058afe991f36474e2e2868
Ocean Fuaga <oceansfuaga@gmail.com>
2024-08-19 21:39:15 -0700

    add state for name, number and people in phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    console.log(people);
+  }, [people]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPeople([...people, { name, number }]);
+    setName("");
+    setNumber("");
+  };
+
   return (
     <>
-      <form action="submit" method="post">
+      <form onSubmit={handleSubmit} method="post">
         <div className="input">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="input">
           <label htmlFor="name">Phone number</label>
@@ -16,15 +37,29 @@ function App() {
             typeof="tel"
             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
             required
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
           />
         </div>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
       <div className="phonebook">
         <h2>Phonebook</h2>
         <div className="phonebook-list">
           <ul>
-            <li>test</li>
+            {people.map((person, index) => (
+              <li key={index}>
+                {person.name} {person.number}{" "}
+                <button
+                  className="delete-btn"
+                  onClick={() => {
+                    setPeople(people.filter((p) => p.name !== person.name));
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
